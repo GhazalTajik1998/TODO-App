927a971b3b0511e3e4f7225c500107acf999b97e
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-01-30 15:45:40 +0330

    Filters and searches added

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,78 +6,30 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .serializers import UserSerializer, TaskListSerializer, TaskCreateSerializer, TaskSerializer
 # from .permissions import IsAuthorPermission
 
 
 
 from todo.models import Task
-# Create your views here.
 
-# class UserApiView(ListCreateAPIView):
-#     permission_classes = [IsAuthenticated]
-
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-    
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# class TaskApiView(ListCreateAPIView):
-#     permission_classes = [IsAuthenticated]
 
-#     def get_serializer_class(self):
-#         if self.request.method == 'POST':
-#             return TaskCreateSerializer
-#         return TaskListSerializer
-
-#     def get_queryset(self):
-#         user = self.request.user
-#         tasks = Task.objects.filter(user=user)
-#         return tasks
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
-# class TaskDetailApiView(RetrieveUpdateDestroyAPIView):
-#     queryset = Task.objects.all()
-#     serializer_class = TaskListSerializer
-#     permission_classes = [IsAuthenticated]
-#     lookup_field = 'id'
-
-#     def get_queryset(self):
-#         user = self.request.user
-#         tasks = Task.objects.filter(user=user)
-#         return tasks
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
-    
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user__username']
     def get_serializer_class(self):
         if self.request.method in ['GET']:
             return TaskListSerializer
         else:
             return TaskCreateSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Task.objects.filter(user=user)
-        return queryset 
-# class TaskViewSet(ViewSet):
-#     def list(self, request):
-#         queryset = Task.objects.filter(user=request.user)
-#         serializer = TaskListSerializer(queryset, many=True)
-#         return Response(serializer.data)
-
-    
-    
-#     def retrieve(self, request, pk=None):
-#         queryset = Task.objects.all()
-#         task = get_object_or_404(queryset, pk=pk)
-#         serializer = TaskListSerializer(task)
-        
-#         return Response(serializer.data)
 
 
 
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'todo.apps.TodoConfig',
     'api.apps.ApiConfig',
 ]
@@ -138,5 +139,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
