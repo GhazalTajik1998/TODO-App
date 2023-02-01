18ab239519dcc9252ed68934cb564ec53e367d97
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-02-01 15:28:57 +0330

    Sorting by priority, filtering by being completed

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,4 +1,11 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
+
+class IsAuthorOfReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj == request.user
 
 
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,31 +5,51 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
-
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import UserSerializer, TaskListSerializer, TaskCreateSerializer, TaskSerializer
-# from .permissions import IsAuthorPermission
+from .permissions import IsAuthorOfReadOnly
 
 
 
 from todo.models import Task
 
 class UserViewSet(ModelViewSet):
+    permission_classes = [IsAuthorOfReadOnly,IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_update(self, serializer):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied(" You can not perform this")
+        serializer.save()
 
 class TaskViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
     queryset = Task.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['user__username']
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ['priority']
+    # filterset_fields = ['completed', 'priority']
+    ordering = ['priority']
+    def get_queryset(self):
+        user = self.request.user
+        if user is not None:    
+            queryset = Task.objects.filter(user=user)
+        return queryset
+
     def get_serializer_class(self):
         if self.request.method in ['GET']:
             return TaskListSerializer
         else:
             return TaskCreateSerializer
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
 
 
 
